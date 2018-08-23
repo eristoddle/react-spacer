2ac0236ddb9e98e0781d9e8afe88bdfa7a73d88d
Stephan Miller <stephanmil@gmail.com>
2018-08-22 19:26:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-// export class Spacer extends Component {
-  
-//   render() {
-//     return (
-//       <div>
-      
-//       </div>
-//     )
-//   }
-// }
+export class Spacer extends PureComponent { 
+  render() {
+    const style = {
+      width: (typeof props.width === 'number' ? `${props.width}px` : (props.width || '1px')),
+      height: (typeof props.height === 'number' ? `${props.height}px` : (props.height || '1px')),
+      flexGrow: props.grow,
+      flexShrink: props.shrink
+    }
+    return (
+      <div style=style>
+        {props.children}
+      </div>
+    )
+  }
+}
 
 export const Spacer = (props) => {
   const style = {
